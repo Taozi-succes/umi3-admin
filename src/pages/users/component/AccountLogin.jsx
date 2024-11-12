e4e6e143d65949e5c9becce4cef9c2abc6993797
Huang Tao <17361010132@163.com>
2024-11-13 00:28:05 +0800

    feat:新增账号密码登录 验证码校验

--- src/pages/users/component/AccountLogin.jsx
+++ src/pages/users/component/AccountLogin.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import iconMap from 'components/iconMap';
-function AccountLogin({ Input, FormItem }) {
+function AccountLogin({ Input, FormItem, Button, form, initCode }) {
+  useEffect(() => {
+    initCode();
+  }, []);
   return (
     <>
-      <FormItem>
+      <FormItem
+        name="username"
+        rules={[{ required: true, message: '请输入用户名' }]}
+      >
         <Input prefix={iconMap.userIcon} placeholder="請輸入用戶名"></Input>
       </FormItem>
-      <FormItem>
+      <FormItem
+        name="password"
+        rules={[{ required: true, message: '请输入密码' }]}
+      >
         <Input prefix={iconMap.passWordIcon} placeholder="請輸入密碼"></Input>
       </FormItem>
     </>
